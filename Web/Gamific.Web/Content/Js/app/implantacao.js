abeb1086eda33c20b132b8bf4f7406b6de53aca6
marco@duplov.com.br <marco@duplov.com.br>
2017-08-16 15:01:41 -0300

    Wizard

---
+++ Web/Gamific.Web/Content/Js/app/implantacao.js
@@ -0,0 +1,27 @@
+﻿function CheckChange(value) {
+
+    if ($('#' + value).is(":checked")) {
+        if ($('#DaysOfWeek').val() != "") {
+            $('#DaysOfWeek').val($('#DaysOfWeek').val() + "," + value);
+        }
+        else {
+            $('#DaysOfWeek').val(value);
+        }
+    }
+    else {
+        var daysOfWeek = $('#DaysOfWeek').val().split(',');
+        var daysChecked = "";
+
+        daysOfWeek.forEach(function (val, key) {
+            if (val != value && val != "") {
+                if (key != 0) {
+                    daysChecked += ",";
+                }
+                daysChecked += val;
+            }
+        });
+
+        $('#DaysOfWeek').val(daysChecked);
+    }
+
+}
